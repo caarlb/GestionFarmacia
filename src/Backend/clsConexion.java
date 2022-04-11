4e9a9d810a6a9856166c2d992f7a1a4b580e6a7d
caarlb <s.caarlb@gmail.com>
2022-04-10 21:41:14 -0600

    creation conexion class

---
+++ src/Backend/clsConexion.java
@@ -0,0 +1,27 @@
+
+package Backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author caarlb
+ */
+public class clsConexion {
+    
+    public static Connection getConexion() {
+        String Cadena = "jdbc:sqlserver://localhost:1433;databaseName=BD_FARMACIA;user=sa;password=Umg$19;loginTimeout=30";
+        try {
+            Connection con = DriverManager.getConnection(Cadena);
+            return con;
+        } catch (SQLException ex) {
+            System.out.println("Erro: " + ex.toString());
+            return null;
+        } finally {
+        }
+
+    }
+    
+}
